23d8f8cad6d538ada9b5a3a40b4a21b9e30661c5
Kevin Eboka <nonso.eboka@gmail.com>
2024-08-18 09:18:40 +0100

    result pattern implementation in progress

---
+++ src/Modules/Events/Evently.Modules.Events.Domain/Abstractions/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace Evently.Modules.Events.Domain.Events;
+
+public class Result
+{
+    private Result(bool isSucess, Error error)
+    {
+        IsSucess = isSucess;
+        Error = error;
+    }
+
+    public bool IsSucess { get; }
+    public bool IsFailure => !IsSucess;
+    public Error Error { get; }
+}
